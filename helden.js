93252bdd84acab48a08fbec462d25d8ca8549f23
Cornell van der Straaten <30183@ma-web.nl>
2020-03-05 21:37:18 +0100

    JQUERY opdracht completed

---
+++ helden.js
@@ -0,0 +1,32 @@
+$(document).ready( function() {
+    // test
+    // $('main').hide(3000).delay(1000).show(5000);
+
+    // Mouse events
+    $('figure img').mouseenter( function() {
+        $(this).fadeTo(1000, .1);
+    }).mouseleave( function() {
+        $(this).fadeTo(1000, 1);
+    }).click( function() {
+        $(this).next().toggle(700);
+    })
+
+    $('#clickMe').click( function() {
+       alert("SURPRISE!");
+    })
+
+    $("#hideMain").click( function() {
+       $('main').slideToggle(1000, 'swing')
+    });
+    // footer
+
+    $('footer').click( function() {
+        $(this).animate( {
+            top: '-=100px',
+            'padding-top': '+=200px'
+        }, 800);
+    }).dblclick( function() {
+        $(this).fadeTo(10000, -0.1)
+    })
+
+});
